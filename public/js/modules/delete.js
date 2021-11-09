44cb33ea50e5d7cde48fc1f12279cc84cde2e95f
Jonathan <jonxplod@outlook.fr>
2021-11-10 00:49:43 +0100

    Fin Function Delete/validé

---
+++ public/js/modules/delete.js
@@ -0,0 +1,18 @@
+export let deleteCheck = (e) => {
+    let item = e.target;
+    
+    // supprimer todo
+    if (item.classList[0] === "trash-btn") {
+        let todo = item.parentElement;
+        // annimation delete
+        todo.classList.add("fall")
+        todo.addEventListener("transitionend", () => {
+            todo.remove();
+        })
+    }
+    // complite todo
+    if (item.classList[0] === "complete-btn") {
+        let todo = item.parentElement;
+        todo.classList.toggle("completed")
+    }
+    }
---
+++ public/js/modules/input.js
@@ -0,0 +1,34 @@
+let todoInput = document.querySelector(".todo-input");
+let todoButton = document.querySelector(".todo-button");
+let todoList = document.querySelector(".todo-list");
+export let addTodo = (event) =>{
+
+    event.preventDefault();
+    // crée Div.todo
+    let todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // crée Li
+    let newTodo = document.createElement("li")
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo)
+
+    // Button Chek
+    let completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class = "fas fa-check"></i>'
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    // Button poubelle
+    let trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // ajoute dans la todoList
+    todoList.appendChild(todoDiv)
+    
+    // Clear la valeur input
+    todoInput.value = "";
+}
